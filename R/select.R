10493d70d5ba073ebbab7888a57cd2c76b78ef57
Sandra Hansen <au587520@d52022.eduroam.net.au.dk>
2023-08-24 10:43:11 +0200

    First function of package

---
+++ R/select.R
@@ -0,0 +1,16 @@
+#' Subset columns of a data frame
+#'
+#' @param x A data frame
+#' @param var Either a vector of column indices or variable names or logicals
+#'
+#' @return a subsetted data frame
+#' @export
+#'
+#' @examples
+#' select(iris, 1:2)
+#' select(iris, "Species")
+#' select(iris, c(TRUE, FALSE, FALSE, TRUE, FALSE))
+select <- function(x, var) {
+  x[var]
+}
+
